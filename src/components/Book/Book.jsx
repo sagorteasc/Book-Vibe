c6606eed56acbe94dd1a6ad6b01bfbd54a25086a
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2026-03-21 22:29:20 +0600

    fix card alignment issue

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -8,18 +8,18 @@ const Book = ({ bookData }) => {
 
     return (
         <div className="card shadow-sm border bg-base-100 text-white border-white w-full">
-            <Link to={`/book/${bookId}`}>
+            <Link className="flex flex-col h-full" to={`/book/${bookId}`}>
                 <figure className="bg-[#F3F3F3] rounded-2xl m-6 p-5">
                     <img src={image} alt={bookName} className="w-40 h-52 rounded-xl" />
                 </figure>
-                <div className="card-body grow">
+                <div className="card-body flex flex-col">
                     <div className="flex gap-2 items-center">
                         {
                             tags.map((tag, idx) => <button className="btn font-medium bg-[#23BE0A25] text-[#23BE0A] w-fit border-none rounded-4xl shadow-none" key={idx}>{tag}</button>)
                         }
                     </div>
                     <h2 className="Titles font-bold text-2xl">{bookName}</h2>
-                    <p className="font-medium">By: {author}</p>
+                    <p className="font-medium opacity-80 h-12 flex-1">By: {author}</p>
                     <hr className="border-dashed mt-2" />
                 </div>
                 <div className="flex justify-between px-7 pb-7">
@@ -29,7 +29,6 @@ const Book = ({ bookData }) => {
                         <p className="font-medium">{rating}</p>
                         <IoIosStarOutline></IoIosStarOutline>
                     </div>
-
                 </div>
             </Link>
         </div>
